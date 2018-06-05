05ff9d8dba0ed5a389005be05283783894021cfe
Mark Myers <mmyers@squaremouth.com>
2018-06-05 19:35:48 -0400

    Add docker-compose file for using in development and rename to d_s
    This pull request adds a `docker-compose.yml` to be used for development
    and renames everything to `d_s`.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -8,33 +8,33 @@
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
- * @package _s
+ * @package d_s
  */
 
 /**
  * Set up the WordPress core custom header feature.
  *
- * @uses _s_header_style()
+ * @uses d_s_header_style()
  */
-function _s_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( '_s_custom_header_args', array(
+function d_s_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'd_s_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
-		'wp-head-callback'       => '_s_header_style',
+		'wp-head-callback'       => 'd_s_header_style',
 	) ) );
 }
-add_action( 'after_setup_theme', '_s_custom_header_setup' );
+add_action( 'after_setup_theme', 'd_s_custom_header_setup' );
 
-if ( ! function_exists( '_s_header_style' ) ) :
+if ( ! function_exists( 'd_s_header_style' ) ) :
 	/**
 	 * Styles the header image and text displayed on the blog.
 	 *
-	 * @see _s_custom_header_setup().
+	 * @see d_s_custom_header_setup().
 	 */
-	function _s_header_style() {
+	function d_s_header_style() {
 		$header_text_color = get_header_textcolor();
 
 		/*
